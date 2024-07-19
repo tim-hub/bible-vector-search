cc1cb822c0ab23610ef003e2009a1185bc17ba24
tim-hub <tim-hub@users.noreply.github.com>
2024-07-20 11:43:31 +1200

    fix type error

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,13 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
+        <Providers
+          themeProps={{
+            attribute: "class",
+            defaultTheme: "light",
+            children: children,
+          }}
+        >
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
