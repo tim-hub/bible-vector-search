a06506a631e04c3b7d0470cd47876a4c17e011c4
tim-hub <tim-hub@users.noreply.github.com>
2024-07-20 20:44:53 +1200

    add more content

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -3,7 +3,9 @@ import { title } from "@/components/primitives";
 export default function DocsPage() {
   return (
     <div>
-      <h1 className={title()}>Docs</h1>
+      <h1 className={title()}>API Docs</h1>
+
+      <p>Work in progress</p>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,40 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import { title } from "@/components/primitives";
+import NextLink from "next/link";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-lg text-center justify-center">
-        <h1 className={title()}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
+        <h1 className={title()}>Search&nbsp;</h1>
+        <h1 className={title({ color: "violet" })}>Bible Verse&nbsp;</h1>
         <br />
-        <h1 className={title()}>
-          websites regardless of your design experience.
-        </h1>
-        <h2 className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </h2>
+        <h1 className={title()}>through meaning, not just words.</h1>
+        {/*<h2 className={subtitle({ class: "mt-4" })}>*/}
+        {/*  Beautiful, fast and modern React UI library.*/}
+        {/*</h2>*/}
       </div>
 
       <div className="flex gap-3">
-        <Link
-          isExternal
+        <NextLink
           className={buttonStyles({
             color: "primary",
             radius: "full",
             variant: "shadow",
           })}
-          href={siteConfig.links.docs}
+          href={"/search"}
         >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
+          Get Started
+        </NextLink>
       </div>
 
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+      {/*<div className="mt-8">*/}
+      {/*  <Snippet hideCopyButton hideSymbol variant="bordered">*/}
+      {/*    <span>*/}
+      {/*      Get started by editing <Code color="primary">app/page.tsx</Code>*/}
+      {/*    </span>*/}
+      {/*  </Snippet>*/}
+      {/*</div>*/}
     </section>
   );
 }
