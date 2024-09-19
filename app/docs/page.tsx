fedcdf504012a43f5caf78d794227fcf23e3d708
tim-hub <tim-hub@users.noreply.github.com>
2024-09-19 18:11:29 +1200

    add doc page

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -1,11 +1,27 @@
 import { title } from "@/components/primitives";
+import {Code} from "@nextui-org/code";
 
 export default function DocsPage() {
   return (
     <div>
       <h1 className={title()}>API Docs</h1>
 
-      <p>Work in progress</p>
+        <div className={"text-left my-4"}>
+            We have a very simply and straightforward API. You can use it to search
+            for Bible verses.
+            <br/>
+            See example below:
+            <br/>
+            <br/>
+            <br/>
+
+
+            <h4>For example,</h4>
+            <Code>
+                https://bible-search.antioch.tech/api/search?verse_query='God is love'
+            </Code>
+        </div>
+
     </div>
   );
 }
